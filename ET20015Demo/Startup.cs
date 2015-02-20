08b5a1697d36fcf8c6cd8e0e4c9d37d4e8ada519
vip32 <32github32>
2015-02-20 15:40:51 +0100

    v1

---
+++ ET20015Demo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ET20015Demo.Startup))]
+namespace ET20015Demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
